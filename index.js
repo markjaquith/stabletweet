eea252f1b560cce9679b0a7d455dc79c359b1464
Mark Jaquith <mark@jaquith.me>
2022-04-07 18:48:27 -0400

    Prettier

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-const VERSION = 3;
-const TTL_HOURS = 6;
+const VERSION = 3
+const TTL_HOURS = 6
 
 addEventListener('fetch', event => {
 	event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
-	const requestUrl = new URL(request.url);
+	const requestUrl = new URL(request.url)
 
 	if (!requestUrl.pathname.startsWith('/exists')) {
 		return new Response('404', {
 			status: 404,
 			headers: {
 				'content-type': 'text/plain',
-			}
+			},
 		})
 	}
 
 	let tweetUrl = requestUrl.searchParams.get('url') || null
-	
+
 	if (!tweetUrl) {
-		return new Response('You must pass in a url parameter with the URL of the tweet', {
-			status: 404,
-			headers: {
-				'content-type': 'text/plain',
-			}
-		})
+		return new Response(
+			'You must pass in a url parameter with the URL of the tweet',
+			{
+				status: 404,
+				headers: {
+					'content-type': 'text/plain',
+				},
+			},
+		)
 	}
 
 	tweetUrl = tweetUrl.split('?').shift()
@@ -35,14 +38,14 @@ async function handleRequest(request) {
 	const headers = {
 		'Access-Control-Allow-Origin': '*',
 	}
-	
+
 	if (exists) {
-		return new Response("true", {
+		return new Response('true', {
 			status: 200,
 			headers,
 		})
 	} else {
-		return new Response("false", {
+		return new Response('false', {
 			status: 404,
 			headers,
 		})
@@ -51,11 +54,18 @@ async function handleRequest(request) {
 
 async function tweetExists(tweetUrl) {
 	const parsed = new URL(tweetUrl)
-	if (!['twitter.com', 'www.twitter.com', 'mobile.twitter.com'].includes(parsed.host)) {
+	if (
+		!['twitter.com', 'www.twitter.com', 'mobile.twitter.com'].includes(
+			parsed.host,
+		)
+	) {
 		return false
 	}
 
-	const tweetId = tweetUrl.split('/').pop().replace(/[^0-9]/g, '')
+	const tweetId = tweetUrl
+		.split('/')
+		.pop()
+		.replace(/[^0-9]/g, '')
 	console.log({ tweetId })
 
 	if (`${tweetId}`.length < 10) {
@@ -67,9 +77,13 @@ async function tweetExists(tweetUrl) {
 	if (exists !== null) {
 		exists = !!parseInt(exists, 10)
 	} else {
-		const response = await fetch(`https://publish.twitter.com/oembed?url=${tweetUrl}`)
+		const response = await fetch(
+			`https://publish.twitter.com/oembed?url=${tweetUrl}`,
+		)
 		exists = response.ok
-		await TWEET_EXISTS.put(VERSION + ':' + tweetId, exists ? "1" : "0", { expirationTtl: 60 * 60 * TTL_HOURS })
+		await TWEET_EXISTS.put(VERSION + ':' + tweetId, exists ? '1' : '0', {
+			expirationTtl: 60 * 60 * TTL_HOURS,
+		})
 	}
 
 	return exists
