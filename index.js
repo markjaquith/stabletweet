3c84023f7f126071f124459d89f7eca066883a01
Mark Jaquith <mark@jaquith.me>
2022-04-07 13:09:29 -0400

    Add caching

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { decode } from 'html-entities'
 
+const VERSION = 1;
+
 function html(content) {
 	return `
 <!DOCTYPE html>
@@ -22,7 +24,6 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
 	const requestUrl = new URL(request.url);
 	const tweetUrl = requestUrl.searchParams.get('url') || null
-
 	if (!tweetUrl) {
 		return new Response('You must pass in a url parameter with the URL of the tweet', {
 			status: 404,
@@ -32,43 +33,55 @@ async function handleRequest(request) {
 		})
 	}
 
-	console.log(`Fetching ${tweetUrl}`)
-	/*
-	<meta content="2022-03-29T10:36:27.000Z" itemProp="datePublished"/>
-	<meta content="https://twitter.com/kevinmarks/status/1508754955108241414" itemProp="url"/>
-	<meta content="https://twitter.com/kevinmarks/status/1508754955108241414" itemProp="mainEntityOfPage"/>
-	<div itemProp="author" itemscope="" itemType="https://schema.org/Person">
-	<meta content="57203" itemProp="identifier"/>
-	<meta content="kevinmarks" itemProp="additionalName"/>
-	<meta content="Kevin Marks" itemProp="givenName"/>
-	*/
-	// <meta data-rh="true" content="“hi @TwitterEng - you broke the fallback case in the tweet embedding js for deleted tweets. Previously they would not be decorated and show the &amp;lt;blockquote&amp;gt; html version. Now they&#x27;re turned into empty white boxes. Do you want us to go back to screenshots?”" property="og:description"/>
-	const tweet = await fetch(tweetUrl, {
-		headers: {
-			'user-agent': 'googlebot',
-		}
-	})
-	console.log('Got Tweet')
+	const {
+		handle,
+		givenName,
+		date,
+		tweetText,
+	} = await getTweet(tweetUrl)
 
-	let htmlPage = await tweet.text()
-	let tweetText = decode(htmlPage.match(/<meta data-rh="true" content="([^"]+)" property="og:description"/)[1] || '')
-	tweetText = tweetText.substring(1, tweetText.length - 1)
-	console.log('Parsed tweet', tweetText)
-
-	let givenName = htmlPage.match(/content="([^"]+)"\s+itemProp="givenName"/)[1] || ''
-	let handle = htmlPage.match(/content="([^"]+)"\s+itemProp="additionalName"/)[1] || ''
-	let date = htmlPage.match(/content="([^"]+)" itemProp="datePublished"/)[1] || ''
-	date = new Date(date)
-	date = date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })
-	
-
-
-	console.log('Parsed name', givenName, handle)
-// &mdash; Kevin Marks (@kevinmarks) \u003Ca href=\"https:\/\/twitter.com\/kevinmarks\/status\/1508754955108241414?ref_src=twsrc%5Etfw\"\u003EMarch 29, 2022\u003C\/a\u003E\u003C
 	return new Response(html(`<blockquote><p>${tweetText}</p>&mdash; ${givenName} (@${handle}) <a href="${tweetUrl}">${date}</a></blockquote>`), {
 		status: 200,
 		headers: {
 			'Content-Type': 'text/html'
 		},
 	})
+}
+
+async function getTweet(tweetUrl) {
+	console.log(`Fetching ${tweetUrl}`)
+	const tweetId = parseInt(tweetUrl.split('?').shift().split('/').pop(), 10)
+	console.log({ tweetId })
+	let data = await TWEETS.get(VERSION + ':' + tweetId)
+
+	if (data) {
+		data = JSON.parse(data)
+	} else {	
+		const tweet = await fetch(tweetUrl, {
+			headers: {
+				'user-agent': 'googlebot',
+			}
+		})
+		console.log('Got Tweet')
+
+		let htmlPage = await tweet.text()
+		let tweetText = decode(htmlPage.match(/<meta data-rh="true" content="([^"]+)" property="og:description"/)[1] || '')
+		tweetText = tweetText.substring(1, tweetText.length - 1)
+		console.log('Parsed tweet', tweetText)
+
+		let givenName = htmlPage.match(/content="([^"]+)"\s+itemProp="givenName"/)[1] || ''
+		let handle = htmlPage.match(/content="([^"]+)"\s+itemProp="additionalName"/)[1] || ''
+		let date = htmlPage.match(/content="([^"]+)" itemProp="datePublished"/)[1] || ''
+		date = new Date(date)
+		date = date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })
+		data = {
+			handle,
+			givenName,
+			date,
+			tweetText,
+		}
+		await TWEETS.put(VERSION + ':' + tweetId, JSON.stringify(data))
+	}
+
+	return data
 }
